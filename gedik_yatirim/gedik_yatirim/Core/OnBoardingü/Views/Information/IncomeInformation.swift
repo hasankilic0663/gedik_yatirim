b64ad2a5a64bab243c877235bb878cbf9e1f4128
hasankilic <hasankilic0663@hotmail.com>
2024-08-15 13:13:43 +0300

    gelirbilgileri ve personoınf eklendı yapıldı

---
+++ gedik_yatirim/gedik_yatirim/Core/OnBoardingü/Views/Information/IncomeInformation.swift
@@ -0,0 +1,18 @@
+//
+//  IncomeInformation.swift
+//  gedik_yatirim
+//
+//  Created by Hasan on 15.08.2024.
+//
+
+import SwiftUI
+
+struct IncomeInformation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IncomeInformation()
+}
